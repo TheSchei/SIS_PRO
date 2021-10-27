f161657f2e32123de9cdab2ec97172684ab522f1
Paweł Jasiński <pawel.jasinski998@gmail.com>
2021-10-27 23:48:07 +0200

    Added almost empty basic classes

---
+++ Chromosome.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS_PRO
+{
+    class Chromosome
+    {
+        public Chromosome()
+        {
+
+        }
+
+        public void Mutate()
+        {
+
+        }
+
+        private void CalculateCollisions()
+        {
+
+        }
+
+        private void GenerateAllPairs()
+        {
+
+        }
+    }
+}
---
+++ EvolutionAlgorithm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS_PRO
+{
+    class EvolutionAlgorithm
+    {
+        public EvolutionAlgorithm()
+        {
+
+        }
+        public void Run()
+        {
+
+        }
+        public void PrintResult()
+        {
+
+        }
+    }
+}
