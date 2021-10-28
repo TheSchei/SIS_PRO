10d532c03fdaf8cf3f980685968d2fc66e9d4539
Paweł Jasiński <pawel.jasinski998@gmail.com>
2021-10-28 16:57:31 +0200

    another class, may be useful

---
+++ Genome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS_PRO
+{
+    class Genome
+    {
+        private List<Chromosome> Chromosomes;
+        public Genome(int NumberOfChromosomes, int NumberOfGenes)
+        {
+
+        }
+
+        public Chromosome Run()
+        {
+            return null;
+        }
+    }
+}
